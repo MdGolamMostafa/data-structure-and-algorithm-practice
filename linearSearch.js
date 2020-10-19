144544458f6fba89597f9ab88b3a5b8c80e28241
MdGolamMostafa <shakilhero11@gmail.com>
2020-10-19 17:00:33 +0600

    linear search example

---
+++ linearSearch.js
@@ -0,0 +1,8 @@
+const friendsOld = [11,33,44,65,66,67,68,69,70,71];
+for (var i = 0; i < friendsOld.length; i++) {
+    const element = friendsOld[i];
+    if (element == 66) {
+        console.log(i);
+    }
+}
+
