0ffe9c434529b7f400f8f6737a8de3ffa36a589c
MdGolamMostafa <shakilhero11@gmail.com>
2020-10-19 18:43:57 +0600

    binary Search example

--- binarySearch.js
+++ binarySearch.js
@@ -1,2 +1,25 @@
 const friendsOld = [2,4,5,6,7,8,9,10,11,12,13];
 
+function binarySearch (friendsOld,motaMamunOld) {
+    let searchStartOld = 0;
+    let searchEndOld = friendsOld.length - 1;
+    while(searchStartOld <= searchEndOld){
+        mid = Math.floor((searchStartOld+ searchStartOld)/2);
+
+        if(friendsOld[mid] == motaMamunOld){
+            console.log('motaMamun Old is ',motaMamunOld);
+            return mid;
+        }
+        else if(friendsOld[mid] < motaMamunOld){
+            searchStartOld = mid + 1;
+        }
+        else if (friendsOld[mid] > motaMamunOld){
+            searchStartOld = mid - 1;
+        }
+    }
+    return -1;
+}
+let motaMamunOld = 11;
+const searchOld = binarySearch(friendsOld, motaMamunOld);
+
+console.log(searchOld)
--- linearSearch.js
+++ linearSearch.js
@@ -1,8 +1,8 @@
 const friendsOld = [11,33,44,65,66,67,68,69,70,71];
 for (var i = 0; i < friendsOld.length; i++) {
-    const element = friendsOld[i];
-    if (element == 66) {
-        console.log(i);
+    const old = friendsOld[i];
+    if (old == 66) {
+        console.log('Siam Old position is ',i,'number');
     }
 }
 
